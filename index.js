3001f1fdd9b6de275da786c26e2f5872194502ce
B. M. Mahbubul Alam <108087360+phmahbub@users.noreply.github.com>
2022-11-07 07:12:52 +0600

    dotenv

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
 
-//user Id : phmahbub
-//password: bagerhaT1
 
 
 
-const uri = "mongodb+srv://phmahbub:bagerhaT1@cluster0.zvdscch.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zvdscch.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
